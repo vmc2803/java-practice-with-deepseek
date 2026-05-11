5987c7a8205e26dfb28c1d5d5f24825ff5a36a77
Vu Minh Chien <vuminhchien2803@gmail.com>
2026-05-11 18:30:25 +0700

    Sửa lỗi chính tả

--- java-practice/warmup01/WarmUp1.java
+++ java-practice/warmup01/WarmUp1.java
@@ -1,9 +1,9 @@
 package warmup01;
 
 public class WarmUp1 {
-    public static void main(String[] args){
-        for (int i = 1; i<= 10; i++){
-            System.out.println(i + ": Toi dang lam qune voi Java");
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ": Toi dang lam quen voi Java");
         }
     }
 }
