79ada32fabc747db200329cabbb852c6efa1a76f
Vu Minh Chien <vuminhchien2803@gmail.com>
2026-05-11 18:23:34 +0700

    Hoàn thành bài WarmUp1 trong package warmup01

---
+++ java-practice/warmup01/WarmUp1.java
@@ -0,0 +1,9 @@
+package warmup01;
+
+public class WarmUp1 {
+    public static void main(String[] args){
+        for (int i = 1; i<= 10; i++){
+            System.out.println(i + ": Toi dang lam qune voi Java");
+        }
+    }
+}
