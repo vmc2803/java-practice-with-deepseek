8e594dfc91ecdabfe018a11d328ad9c0a7570e14
Vu Minh Chien <vuminhchien2803@gmail.com>
2026-05-12 18:21:12 +0700

    bai tap WarmUp3 voi method va while loop

--- java-practice/warmup3/WarmUp3.java
+++ java-practice/warmup3/WarmUp3.java
@@ -5,12 +5,28 @@ import java.util.Scanner;
 public class WarmUp3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap so n: ");
         int n = scanner.nextInt();
 
-        for (int i = 1; i <= 10; i++) {
+        inThanhNgang();
+        System.out.println("may tinh dang xu ly");
+        inThanhNgang();
+        System.out.println("ket qua la: ");
+        inBangCuuChuongVoiWhile(n);
+
+        scanner.close();
+    }
+
+    public static void inBangCuuChuongVoiWhile(int n) {
+        int i = 1;
+        while (i <= 10) {
             int res = n * i;
             System.out.println(n + " x " + i + " = " + res);
+            i++;
         }
-        scanner.close();
+    }
+
+    public static void inThanhNgang() {
+        System.out.println("=========================================");
     }
 }
