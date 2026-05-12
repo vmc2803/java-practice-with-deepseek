3be5d595d6927b43d35c50ac4a81ce5249884c07
Vu Minh Chien <vuminhchien2803@gmail.com>
2026-05-12 18:00:20 +0700

    bai tap warmup3

---
+++ java-practice/warmup3/WarmUp3.java
@@ -0,0 +1,15 @@
+package warmup3;
+
+import java.util.Scanner;
+
+public class WarmUp3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            int res = n * i;
+            System.out.println(res);
+        }
+    }
+}
